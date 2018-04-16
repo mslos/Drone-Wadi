ca951c714184f1d6e4d8ed807f4cbab8dc6cb71c
dbcarelli <dbcarelli@gmail.com>
2018-04-16 23:43:03 +0400

    Bug fix.

--- anaconda_avionics/utilities/mission_plan_parser.py
+++ anaconda_avionics/utilities/mission_plan_parser.py
@@ -20,7 +20,7 @@ class MissionPlanParser(object):
         self._cmds  = self._autopilot.commands
         self._cmds.download()
         self._cmds.wait_ready()
-        self._number_of_commands = self._cmds.count()
+        self._number_of_commands = self._cmds.count
 
     def extract_waypoints(self):
         """
@@ -29,15 +29,17 @@ class MissionPlanParser(object):
         """
 
         # Read in list of MAVlink commands from the pixhakw.
-        cmd_index = 2
+        cmd_index = 1
         # command 0 is always Mission Start
         # command 1 is always Takeoff
         while (cmd_index < self._number_of_commands):
         # TODO: distinguish between landing, takeoff and obstacle avoidance
 
             # get the command ID
-            cmd = self._vehicle._wploader.wp(cmd_index)
 
+
+            cmd = self._autopilot._wploader.wp(cmd_index)
+            print cmd.command
             # determine if the command indicates a data station
             if (cmd.command == 16): # 16 corresponds to MAV_CMD_NAV_WAYPOINT
                 new_waypoint = LandingWaypoint(cmd_index, cmd.x, cmd.y, cmd.z)
@@ -51,8 +53,8 @@ class MissionPlanParser(object):
                 new_waypoint = LandingWaypoint(cmd_index, cmd.x, cmd.y, cmd.z)
                 self._landing_waypoints.append(new_waypoint)
 
-            elif (cmd.command == 31010): # corresponds to a data station!
-                new_data_station = DataStaion(cmd.x, cmd.y, cmd.z, cmd.param1)
+            elif (cmd.command == 201): # corresponds to a data station! MAV_CMD_DO_SET_ROI
+                new_data_station = DataStation(cmd.x, cmd.y, cmd.z, cmd.param4)
                 self._data_stations.append(new_data_station)
                 # the next waypoint is MAV_CMD_NAV_WAYPOINT
                 # at the same location
@@ -63,6 +65,8 @@ class MissionPlanParser(object):
                 # we should throw some king of global error for cmds that
                 # the drone is not expecting
                 pass
+
+            cmd_index += 1
 
         # Return the results
         return self._data_stations, self._landing_waypoints
@@ -76,3 +80,18 @@ if __name__ == '__main__':
     print 'Connection succesful!'
 
     parser = MissionPlanParser(vehicle)
+    parser.extract_waypoints()
+
+    print "Landing Waypoints: "
+    for waypoint in parser._landing_waypoints:
+        print "Lat: ", waypoint.lat, "Lon: ", waypoint.lon, "Alt: ", waypoint.alt
+
+    print "\n----------------------\n"
+
+    for ds in parser._data_stations:
+        print "Data Station: ", ds.identity
+        print "    Lat: ", ds.lat, "Lon: ", ds.lon, "Alt: ", ds.alt
+        print "    Timeout Event: ", ds.timeout
+        print "    Drone Arrived: ", ds.drone_arrived
+        print "    Download Start: ", ds.download_started
+        print "    Download Complete: ", ds.download_complete
