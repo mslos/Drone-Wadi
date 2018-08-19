4768d444f56eaa8941b711b1c82788b5dc9e7340
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-08-19 13:32:41 -0700

    Ground test

--- avionics/services/navigation/navigation.py
+++ avionics/services/navigation/navigation.py
@@ -103,49 +103,69 @@ class Navigation(object):
             current_waypoint = self.__vehicle.commands.next-1
             logging.debug("Current waypoint: %s", current_waypoint)
 
-            # If we are en route to a data station (marked as LOITER waypoint followed by an ROI)
-            if (waypoint_count-current_waypoint > 1) and \
-                (waypoints[current_waypoint].command == self.LOITER_WAYPOINT_COMMAND) and \
-                (waypoints[current_waypoint+1].command == self.ROI_WAYPOINT_COMMAND):
-                # By default, PX4 uses floats. We use strings (of rounded integers) for data station IDs
-                data_station_id = str(int(waypoints[current_waypoint+1].param3))
+            # Pass the data station ID to the data station handler
+            self.rx_queue.put("321")
+            new_ds.set()
 
-                logging.info("En route to data station: %s", data_station_id)
+            time.sleep(3)
+            wakeup_event.set()
 
-                # Pass the data station ID to the data station handler
-                self.rx_queue.put(data_station_id)
-                new_ds.set()
+            time.sleep(20)
+            download_event.set()
 
-                # Give the DataStationHandler some time to kick off the download
-                time.sleep(5)
-
-                self.wait_flight_distance(5000, waypoints[current_waypoint], data_station_id)
-                logging.info("Beginning XBee wakeup from data station %s...", data_station_id)
-
-                # Tell the data station handler to begin wakeup
-                wakeup_event.set()
-
-                self.wait_flight_distance(1000, waypoints[current_waypoint], data_station_id)
-                logging.info("Beginning data download from data station %s...", data_station_id)
-
-                # Tell the data stataion handler to begin download
-                download_event.set()
-
-                while is_downloading.is_set():
+             while is_downloading.is_set():
                     logging.debug("Downloading...")
                     time.sleep(3)
 
-                wakeup_event.clear()
-                download_event.clear()
+            wakeup_event.clear()
+            download_event.clear()
 
-                # Skip the ROI point
-                next_waypoint = current_waypoint+2
-                logging.info("Done downloading. Moving on to waypoint %i...", (next_waypoint+1))
-                self.__vehicle.commands.next = next_waypoint
+            self.stop()
 
-            else:
-                logging.debug("Not at data station...")
-                time.sleep(3)
+
+            # # If we are en route to a data station (marked as LOITER waypoint followed by an ROI)
+            # if (waypoint_count-current_waypoint > 1) and \
+            #     (waypoints[current_waypoint].command == self.LOITER_WAYPOINT_COMMAND) and \
+            #     (waypoints[current_waypoint+1].command == self.ROI_WAYPOINT_COMMAND):
+            #     # By default, PX4 uses floats. We use strings (of rounded integers) for data station IDs
+            #     data_station_id = str(int(waypoints[current_waypoint+1].param3))
+            #
+            #     logging.info("En route to data station: %s", data_station_id)
+            #
+            #     # Pass the data station ID to the data station handler
+            #     self.rx_queue.put(data_station_id)
+            #     new_ds.set()
+            #
+            #     # Give the DataStationHandler some time to kick off the download
+            #     time.sleep(5)
+            #
+            #     self.wait_flight_distance(5000, waypoints[current_waypoint], data_station_id)
+            #     logging.info("Beginning XBee wakeup from data station %s...", data_station_id)
+            #
+            #     # Tell the data station handler to begin wakeup
+            #     wakeup_event.set()
+            #
+            #     self.wait_flight_distance(1000, waypoints[current_waypoint], data_station_id)
+            #     logging.info("Beginning data download from data station %s...", data_station_id)
+            #
+            #     # Tell the data stataion handler to begin download
+            #     download_event.set()
+            #
+            #     while is_downloading.is_set():
+            #         logging.debug("Downloading...")
+            #         time.sleep(3)
+            #
+            #     wakeup_event.clear()
+            #     download_event.clear()
+            #
+            #     # Skip the ROI point
+            #     next_waypoint = current_waypoint+2
+            #     logging.info("Done downloading. Moving on to waypoint %i...", (next_waypoint+1))
+            #     self.__vehicle.commands.next = next_waypoint
+            #
+            # else:
+            #     logging.debug("Not at data station...")
+            #     time.sleep(3)
 
     def stop(self):
         logging.info("Stoping navigation...")
